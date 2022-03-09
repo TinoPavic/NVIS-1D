7c6ba09b7bd8081a2c0aa72c4186ef01afcb6fc5
TinoPavic <74692205+TinoPavic@users.noreply.github.com>
2022-03-10 07:01:11 +1100

    Add files via upload

--- nvis.js
+++ nvis.js
@@ -128,17 +128,17 @@ function calcfoF2(nvis) {  // foF2 daily minimum   min 2.0, lat+0.5, fold at S 2
 function latestfoF2(nvis) {  // current foF2 min max from Ionosondes
   var t=nvis.lat;
   var f1=3.0, f3=6.0;          // Mawson Station, Antarctica   
-  if(t>-50) {f1=3.5; f3=7.0; } // Hobart
-  if(t>-40) {f1=3.4; f3=7.5; } // Vic Mid CBR and Hobart
-  if(t>-36) {f1=3.4; f3=8.0; } // Canberra
+  if(t>-50) {f1=3.1; f3=7.6; } // Hobart
+  if(t>-40) {f1=3.4; f3=8.4; } // Vic Mid CBR and Hobart
+  if(t>-36) {f1=3.7; f3=9.0; } // Canberra
   if(t>-34.5) {f1=2.7; f3=8.7; } // Camden, Sydney
-  if(t>-32.5) {f1=2.7; f3=8.7; } // Perth
-  if(t>-31) {f1=3.7; f3=9.5; } // Brisbane
-  if(t>-23) {f1=3.5; f3=11.5; } // Townsville
-  if(t>-15) {f1=3.8; f3=12.5; } // Darwin
+  if(t>-32.5) {f1=3.7; f3=9.2; } // Perth
+  if(t>-31) {f1=4.0; f3=10.5; } // Brisbane
+  if(t>-23) {f1=3.5; f3=12.0; } // Townsville
+  if(t>-15) {f1=3.1; f3=13.5; } // Darwin
   f2 = (f1+f3)/2;// adjust f2
   // Mix with prediction
-  var ye=2022, mo=2, da=3;   // date when Ionosonde adjusted  
+  var ye=2022, mo=3, da=10;   // date when Ionosonde adjusted  
   var d1 = ye*365 + mo*30.5 + da;
   var d2 = nvis.year*365 + nvis.month*30.5 + 15; // date prediction in days
   var me=(d2-d1)/90; me=Math.abs(me);
